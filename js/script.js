040affbd8264c105ca13314095a56303183f449d
Heri23F <heri.github.com@gmail.com>
2026-03-18 17:29:33 +0700

    Complete the basic requiment for the project

--- js/script.js
+++ js/script.js
@@ -17,16 +17,45 @@ function createNewDiv (line) {
     }
 
     gridSquare.appendChild(fragment);
-}
 
-createNewDiv(lineInit);
-
-// add event listner to all Square-div
-
-let items = document.querySelectorAll(".square-div");
-items.forEach((item) => {
+    // add event listner to all Square-div
+    let items = document.querySelectorAll(".square-div");
+    items.forEach((item) => {
     item.addEventListener("mouseenter", () => {
         item.classList.add("hover")
     })
 })
 
+}
+
+createNewDiv(lineInit);
+
+
+
+
+
+
+// button for removing all exiting div and create new one
+
+function removeAll (items) {
+    for (let item of items) {
+        item.remove()
+    }
+}
+
+const gridBtn = document.querySelector("#grid-button")
+
+gridBtn.addEventListener("click",  () => {
+    let newLine = prompt("Inser number beetween 1-100")
+    console.log(newLine)
+    console.log(typeof(newLine))
+    if (newLine > 100 || newLine < 0 || !Number(newLine)) {
+        return alert("Please input valid number")
+    }
+
+    let numLine = Number(newLine)
+    let items = document.querySelectorAll(".square-div")
+    removeAll(items);
+    createNewDiv(newLine)
+    
+})
